5bc6e9f9da1314af6de7102cfc67a3a2a85ece76
Jiyoung Baek <vvvv4449@gmail.com>
2023-01-29 18:57:26 +0900

    #문제풀기 30 - 숫자맞추기

--- js/03.js
+++ js/03.js
@@ -1,4 +1,4 @@
-
+// 26
     let tax = (salary, marriage) => {
     let percentage = 0;
     switch(marriage) {
@@ -8,4 +8,22 @@
 }
     return salary * percentage;
 }
+
+//30
+    let catchNumber = (num1) => {
+        while(num1 != num2){
+           // num1 = prompt('1~100 사이 숫자를 입력하시오');
+            if (num1 > num2) {
+                msg = '추측한 숫자가 큽니다.';
+                console.log(msg)}
+            else if (num1 < num2) {
+                msg = '추측한 숫자가 작습니다.';
+                console.log(msg)}
+        }
+
+        msg = ' 빙고! 숫자를 맞췄습니다.';
+        return msg;
+    }
+
+
 
