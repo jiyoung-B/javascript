1e92e3187b9884b5a7bc51c6f3b9e01fd025ea9d
Jiyoung Baek <vvvv4449@gmail.com>
2023-01-30 01:35:33 +0900

    #문제풀기 31, 32

--- js/03.js
+++ js/03.js
@@ -11,19 +11,58 @@
 
 //30
     let catchNumber = (num1) => {
-        while(num1 != num2){
-           // num1 = prompt('1~100 사이 숫자를 입력하시오');
+        for (let i = 0; i <= 2; ++i) {
+            num1 = prompt('1~100 사이 숫자를 입력하시오');
             if (num1 > num2) {
                 msg = '추측한 숫자가 큽니다.';
-                console.log(msg)}
-            else if (num1 < num2) {
+                console.log(msg);
+                document.write(msg);
+                alert('틀렸습니다. 다른 숫자를 입력하시오');
+            } else if (num1 < num2) {
                 msg = '추측한 숫자가 작습니다.';
-                console.log(msg)}
+                console.log(msg);
+                document.write(msg);
+                alert('틀렸습니다. 다른 숫자를 입력하시오');
+            } else {
+                msg = '빙고! 숫자를 맞췄습니다.';
+                console.log(msg);
+                break;
+            }
         }
-
-        msg = ' 빙고! 숫자를 맞췄습니다.';
         return msg;
     }
+
+//31
+    let identifyNumber = (number) => {
+        // msg = '정수입니다';
+        if (number == 0){
+            msg = '0 입니다';
+        }else if (number < 0){
+            msg = '음수입니다';
+        } else { msg = '정수입니다';}
+        return msg;
+    }
+
+//32
+    let checkNumber = (number) => {
+        if(number < 10000) {
+            msg = '10000 미만';
+        }else if(number < 20000){
+            msg = '10000 ~ 20000';
+        }else if(number < 30000){
+            msg = '20000 ~ 30000';
+        }else {msg = '범위 밖 값';}
+
+        return msg;
+    }
+
+//33
+    let cardCheck = (cardNumber) => {
+
+
+    }
+
+//
 
 
 
