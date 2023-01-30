b9221f56c8dd876cf5ff9e9c9d3dec9606c90206
jiyoung-B <catgirl0313@naver.com>
2023-01-30 18:22:13 +0900

    문제풀이 t

--- js/04t.js
+++ js/04t.js
@@ -19,3 +19,127 @@
 
     return msg;
     }
+
+    //#26
+    let computeTax = (isMarried, salary) => {
+        let tax = 0;
+
+        if(isMarried == 'no'){
+            if (salary < 3000) {
+                tax = salary *(10 / 100);
+            } else {
+                tax = salary *(25 / 100);
+            }
+        } else {
+            if(salary < 6000){
+                tax = salary *(15 / 100);
+            }else{
+                tax = salary *(35 / 100);
+            }
+        }
+        return `결혼여부 : ${isMarried}, 연봉: ${salary}, 납부세금: ${tax} 만원`;
+    }
+    //#30
+    let catchNumber = (num2) => {
+        let num1 = 0;
+        let result ='';
+        while (true) {
+            num1 = parseInt(prompt('임의의 숫자 3자리는?'));
+
+            if (num1 > num2) {
+                result = '추측한 숫자가 커요~';
+            } else if (num1 < num2) {
+                result = '추측한 숫자가 작아요~';
+            } else {
+                document.write('빙고! 숫자를 맞췄어요~')
+                break;
+            }
+            console.log(result);
+        }
+    }
+
+    //#32
+    let checkNumber = () => {
+      //  let nums = parseInt(prompt('임의의 숫자는?'));
+        let result = '범위 밖 값'
+
+        if(nums < 10000) {
+            result = '10000 미만';
+        }else if(nums >= 10000 && nums < 20000){
+            result = '10000 ~ 20000미만';
+        }else if(nums >= 20000 && nums < 30000){
+            result = '20000 ~ 30000미만';
+        }
+        return result;
+    }
+
+    // #33
+    let cardCheck = (cardnum) => {
+        // let cardnum = prompt('카드번호는?');
+        let cdnum = cardnum[1] + cardnum[2] + cardnum[3] + cardnum[4] + cardnum[5] ;
+        let result = '잘못된 카드번호 입니다!!';
+        if(cardnum[0] == '4'){
+            result = 'visa - ';
+            if(cdnum == '04825') result += '비씨카드';
+            else if (cdnum == '38676') result += '신한카드';
+            else if (cdnum == '57973') result += '국민은행';
+
+        } else if (cardnum[0] == '5') {
+            result = 'master - ';
+            switch(cdnum) {
+                case '15594' : result += '신한카드'; break;
+                case '24353' : result += '외환카드'; break;
+                case '40926' : result += '국민은행'; break;
+            }
+        } else if (cardnum[0] == '3' && cardnum[1] == '5') {
+            cdnum = cardnum[2] + cardnum[3] + cardnum[4] + cardnum[5] ;
+            result = 'jcb - ';
+            switch(cdnum) {
+                case '6317' : result += 'NH농협카드'; break;
+                case '6901' : result += '신한카드'; break;
+                case '6912' : result += 'KB국민카드'; break;
+            }
+        }
+        return result;
+
+    }
+
+    let checkDayTime = (daytime) => {
+        let result = '';
+
+        switch (daytime){
+            case 'morninghours' : result = '아침시간 (7-12)'; break;
+            case 'midday' :
+            case 'noon' : result = '점심시간 (12-1)'; break;
+            case 'afternoon hours' : result = '오후 (1-6)'; break;
+            case 'evening hours' : result = '저녁시간 (6-9)'; break;
+            case 'night hours' : result = '밤시간 (9-12)'; break;
+            case 'midnight' : result = '자정시간 (12)'; break;
+            case 'early morning hours' : result = '새벽시간 (12-5)'; break;
+            case 'small hours' : result = '새벽 (1-3)'; break;
+            case 'dawn' :
+            case 'daybreak' : result = '해뜰녘 (5-7)'; break;
+            default : result = '잘못 입력하셨습니다.'; break;
+        }
+        return result;
+    }
+
+    // #48
+    let computeInvestment = (account, interest) => {
+        let sum = account;
+        let year = 0;
+        while (true) {
+            if (sum > account * 2) break;
+            sum += sum * (interest / 100);
+            ++year;
+            console.log(year, sum);
+        }
+        return `${year}년 후, ${parseInt(sum)}원`;
+    }
+
+    // #51
+
+
+
+
+
